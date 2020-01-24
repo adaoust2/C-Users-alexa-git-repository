3417f67ead849f1b55ab3e634414cf26d4cb2c0b
Oumar <Diallo.oumar00@gmail.com>
2020-01-24 16:08:35 +0100

    Update class Classement

--- HighScore/src/data/Tournoi.java
+++ HighScore/src/data/Tournoi.java
@@ -1,5 +1,25 @@
 package data;
 
 public class Tournoi {
+	private Groupe groups ;
 
+	public Tournoi(Groupe groups) {
+		super();
+		this.groups = groups;
+	}
+
+	public Groupe getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Groupe groups) {
+		this.groups = groups;
+	}
+
+	@Override
+	public String toString() {
+		return "Tournoi [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
 }
